cb9891f87bf8980caf5183e9e1aeb6dd315b6287
Kevin <airsen40@gmail.com>
2025-10-17 14:33:32 -0300

    First commit

---
+++ src/main/java/dev/spring/ProjetoNinjaFiasco/ProjetoNinjaFiascoApplication.java
@@ -0,0 +1,13 @@
+package dev.spring.ProjetoNinjaFiasco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoNinjaFiascoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoNinjaFiascoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/spring/ProjetoNinjaFiasco/ProjetoNinjaFiascoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.ProjetoNinjaFiasco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoNinjaFiascoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
